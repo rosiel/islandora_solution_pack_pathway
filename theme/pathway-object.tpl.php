50ed999cb44a71010a9dfb8832c14a104ad50d82
Rosie Le Faive <lefaive@gmail.com>
2014-11-21 14:15:20 -0400

    Can view waypoints at /pathway/waypointpid.

--- theme/pathway-object.tpl.php
+++ theme/pathway-object.tpl.php
@@ -2,18 +2,23 @@
 
 /**
  * @file
- *
  * Variables available:
  *
  * $object
  *   The fedora object for this page.
  *
  * $children
- *   The rendered display of children objects.
+ *   The rendered menu of children objects.
+ *
+ * $waypoint_content
+ *   The rendered display of the current islandora waypoint.
  */
 ?>
 <div class="islandora-pathway-object islandora">
-  <div class="pathway_menu">
-    <?php print($children); ?>
+  <div class="pathway-menu">
+    <?php print ($children); ?>
+  </div>
+  <div class="islandora-waypoint-content">
+    <?php print ($waypoint_content); ?>
   </div>
 </div>
