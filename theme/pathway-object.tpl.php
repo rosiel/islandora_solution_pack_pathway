efbad483d663706fdc5da4a0e4dc7621a7f272ba
Rosie Le Faive <lefaive@gmail.com>
2014-11-21 11:03:01 -0400

    added theme function for pathway object

---
+++ theme/pathway-object.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ *
+ * Variables available:
+ *
+ * $object
+ *   The fedora object for this page.
+ *
+ * $children
+ *   The rendered display of children objects.
+ */
+?>
+<div class="islandora-pathway-object islandora">
+  <div class="pathway_menu">
+    <?php print($children); ?>
+  </div>
+</div>
